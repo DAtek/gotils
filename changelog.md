5dbe0826ebcd457aae6ddd261e4ff6dacb068a0f
Attila Dudas <dudasa7@gmail.com>
2023-04-17 18:03:47 +0200

    Improve NewTimeout()

--- changelog.md
+++ changelog.md
@@ -6,12 +6,16 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 
-## [0.1.2] - 2023-04-11
+## [v0.1.3] - 2023-04-17
+### Changed
+- Use time.Timer in `NewTimeout()`
+
+## [v0.1.2] - 2023-04-11
 ### Fixed
 - Make reduce function public
 
-## [0.1.1] - 2023-04-11 [YANKED]
+## [v0.1.1] - 2023-04-11 [YANKED]
 ### Added
 - `func reduce[T any](arr []T, f func(accumulator, currentValue T) T) T`
 
-## [0.1.0] - 2023-04-07
+## [v0.1.0] - 2023-04-07
--- timeout.go
+++ timeout.go
@@ -7,38 +7,30 @@ import (
 const ErrorTimeout = Error("TIMEOUT_ERROR")
 
 type timeout struct {
-	finished bool
-	duration time.Duration
 	ErrorCh  chan error
+	timer    *time.Timer
+	duration time.Duration
 }
 
 func NewTimeout(duration time.Duration) *timeout {
+	errorCh := make(chan error, 1)
 	t := &timeout{
+		ErrorCh:  errorCh,
 		duration: duration,
-		ErrorCh:  make(chan error, 1),
+		timer: time.AfterFunc(duration, func() {
+			errorCh <- ErrorTimeout
+		}),
 	}
 
-	t.start()
 	return t
 }
 
 // Useful in tests
 func NewTimeoutMs(durationMs uint) *timeout {
 	t := NewTimeout(time.Duration(durationMs) * time.Millisecond)
-	t.start()
 	return t
 }
 
 func (t *timeout) Cancel() {
-	t.finished = true
-}
-
-func (t *timeout) start() {
-	go func() {
-		time.Sleep(t.duration)
-		if t.finished {
-			return
-		}
-		t.ErrorCh <- ErrorTimeout
-	}()
+	t.timer.Stop()
 }
