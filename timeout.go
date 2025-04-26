b0a4d8d58f1f495d96daa2e2897503eeaa3e8999
Attila Dudas <dudasa7@gmail.com>
2025-04-26 12:00:37 +0200

    Simplify NewTimeout

--- timeout.go
+++ timeout.go
@@ -6,25 +6,9 @@ import (
 
 const ErrorTimeout Error = "TIMEOUT_ERROR"
 
-type timeout struct {
-	ErrorCh  chan error
-	timer    *time.Timer
-	duration time.Duration
-}
-
-func NewTimeout(duration time.Duration) *timeout {
+func NewTimeout(duration time.Duration) (cancel func(), errorChannel <-chan error) {
 	errorCh := make(chan error, 1)
-	t := &timeout{
-		ErrorCh:  errorCh,
-		duration: duration,
-		timer: time.AfterFunc(duration, func() {
-			errorCh <- ErrorTimeout
-		}),
-	}
-
-	return t
-}
-
-func (t *timeout) Cancel() {
-	t.timer.Stop()
+	timer := time.AfterFunc(duration, func() { errorCh <- ErrorTimeout })
+	cancel = func() { timer.Stop() }
+	return cancel, errorCh
 }
--- timeout_test.go
+++ timeout_test.go
@@ -10,20 +10,20 @@ import (
 
 func TestTimeout(t *testing.T) {
 	t.Run("Timeout returns error after specified time elapsed", func(t *testing.T) {
-		tout := gotils.NewTimeout(1 * time.Millisecond)
+		_, errorChannel := gotils.NewTimeout(1 * time.Millisecond)
 
-		err := <-tout.ErrorCh
+		err := <-errorChannel
 
 		assert.EqualError(t, err, gotils.ErrorTimeout.Error())
 	})
 
 	t.Run("No error if timeout is cancelled", func(t *testing.T) {
-		timeout := gotils.NewTimeout(1 * time.Millisecond)
+		cancel, errorChannel := gotils.NewTimeout(1 * time.Millisecond)
 		var err error
-		go func() { err = <-timeout.ErrorCh }()
+		go func() { err = <-errorChannel }()
 
 		time.Sleep(50 * time.Nanosecond)
-		timeout.Cancel()
+		cancel()
 		time.Sleep(1 * time.Millisecond)
 
 		assert.Nil(t, err)
